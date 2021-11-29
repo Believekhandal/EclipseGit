adb631e850884edb46d33ee81c6ec36611399756
Believe <Believe@Believe-PC>
2021-11-29 17:18:34 +0530

    one change

--- bootdemo/src/main/java/com/example/bootdemo/BootdemoApplication.java
+++ bootdemo/src/main/java/com/example/bootdemo/BootdemoApplication.java
@@ -8,7 +8,7 @@ public class BootdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootdemoApplication.class, args);
-		System.out.println("hello spring boot");
+		System.out.println("hello spring");
 	}
 
 }
